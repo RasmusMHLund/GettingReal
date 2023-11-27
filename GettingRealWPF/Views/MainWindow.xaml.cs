acc588e575f5348d3cec080b49731c8662beb9e1
RasmusRosengaard <rrni58420@edu.ucl.dk>
2023-11-27 12:30:03 +0100

    AddedAbonBtn

--- GettingRealWPF/Views/MainWindow.xaml.cs
+++ GettingRealWPF/Views/MainWindow.xaml.cs
@@ -12,7 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-
+using GettingRealWPF.Views;
 namespace GettingRealWPF
 {
     /// <summary>
@@ -35,6 +35,12 @@ namespace GettingRealWPF
         {
             WindowProdukter windowProdukter = new WindowProdukter();
             windowProdukter.Show();
+        }
+
+        private void BtnAbonnementer_Clik(object sender, RoutedEventArgs e)
+        {
+            WindowAbonnementer windowAbonnementer = new WindowAbonnementer();
+            windowAbonnementer.Show();
         }
     }
 }
