ae933336225014c8b3bda959b91cd0c689315215
Rasmus Lund <Rasmuslund67@gmail.com>
2023-12-06 09:03:31 +0100

    Added basic login system

---
+++ GettingRealWPF/Views/LoginScreen.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace GettingRealWPF
+{
+    /// <summary>
+    /// Interaction logic for LoginScreen.xaml
+    /// </summary>
+    public partial class LoginScreen : Window
+    {
+        public LoginScreen()
+        {
+            InitializeComponent();
+        }
+
+        private void KundeLogin(object sender, RoutedEventArgs e)
+        {
+            KundeView kundeView = new KundeView();
+            kundeView.Show();
+            Close();
+        }
+
+        private void AdminLogin(object sender, RoutedEventArgs e)
+        {
+            MainWindow mainWindow = new MainWindow();
+            mainWindow.Show();
+            Close();
+        }
+    }
+}
