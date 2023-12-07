c63d027f3a12d0471edd1223bc0f55a24bbb514b
Rasmus Lund <Rasmuslund67@gmail.com>
2023-12-07 12:40:58 +0100

    Tried Fixing things

---
+++ GettingRealWPF/Models/Kategorier.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GettingRealWPF.Models.Produkter
+{
+    public class Kategorier
+    {
+        public string NavnType { get; set; }
+
+        public Kategorier(string navnType)
+        {
+            NavnType = navnType;
+        }
+    }
+}
--- GettingRealWPF/Models/Produkter/BærMerchandise.cs
+++ GettingRealWPF/Models/Produkter/BærMerchandise.cs
@@ -6,26 +6,23 @@ using System.Threading.Tasks;
 
 namespace GettingRealWPF.Models.Produkter
 {
-    namespace GettingRealWPF.Models.Produkter
+    public class BærMerchandise : Samling
     {
-        public class BærMerchandise : Samling
+        public BærMerchandise(string navn, string varenummer, double pris) : base()
         {
-            public BærMerchandise(string navn, string varenummer, double pris) : base()
-            {
-                Navn = navn;
-                Pris = pris;
-                Varenummer = varenummer;
-            }
-            public BærMerchandise(string navn, double pris) : this(navn, "", pris)
-            {
-            }
-            public BærMerchandise(string navn) : this(navn, "", 0)
-            {
-            }
-            public BærMerchandise()
-            {
-                // Initialize default values or leave properties uninitialized
-            }
+            Navn = navn;
+            Pris = pris;
+            Varenummer = varenummer;
+        }
+        public BærMerchandise(string navn, double pris) : this(navn, "", pris)
+        {
+        }
+        public BærMerchandise(string navn) : this(navn, "", 0)
+        {
+        }
+        public BærMerchandise()
+        {
+            // Initialize default values or leave properties uninitialized
         }
     }
 }
